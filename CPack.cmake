e0dcf170b617e12dc3e6911412bf32e884edc072
TiilS <75080702+TiilS@users.noreply.github.com>
2024-05-19 16:19:18 +0300

    Update CPack.cmake

--- CPack.cmake
+++ CPack.cmake
@@ -1,33 +1,6 @@
-name: CPack
+set(CPACK_PACKAGE_NAME "RK2")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "RK2 project")
+set(CPACK_PACKAGE_VERSION "1.0.0")
+set(CPACK_GENERATOR "ZIP")
 
-on:
- push:
-   tags:
-     - v**
-
-jobs: 
-
-  build_packages_Linux:
-
-    runs-on: ubuntu-latest
-
-    steps:
-    - uses: actions/checkout@v3
-
-	- name: Config ConceptualExample
-	  run: cmake ${{github.workspace}} -B ${{github.workspace}}/build -D PRINT_VERSION=${GITHUB_REF_NAME#v}
-
-	- name: Build ConceptualExample
-	  run: cmake --build ${{github.workspace}}/build1
-
-    - name: Build package
-      run: cmake --build ${{github.workspace}}/build1 --target package
-
-    - name: Build source package
-      run: cmake --build ${{github.workspace}}/build1 --target package_source
-
-    - name: Make a release
-      uses: ncipollo/release-action@v1.10.0
-      with:
-        artifacts: "build/*.deb,build/*.tar.gz,build/*.zip"
-        token: ${{ secrets.GITHUB_TOKEN }}
+include(CPack)
