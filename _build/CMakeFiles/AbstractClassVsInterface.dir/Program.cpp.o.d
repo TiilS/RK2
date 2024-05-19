2b86bcf1ddfa6fd2dda59b29afda10d0e40c7067
TiilS <ilyaagildin@yandex.ru>
2024-05-19 13:31:35 +0300

    Task 3

---
+++ _build/CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o: \
+  /Users/main/TiilS/workspace/RK2/Program.cpp
---
+++ _build/CMakeFiles/AbstractClassVsInterface.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AbstractClassVsInterface"
+  "AbstractClassVsInterface.pdb"
+  "CMakeFiles/AbstractClassVsInterface.dir/ConceptualExample.cpp.o"
+  "CMakeFiles/AbstractClassVsInterface.dir/ConceptualExample.cpp.o.d"
+  "CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o"
+  "CMakeFiles/AbstractClassVsInterface.dir/Program.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AbstractClassVsInterface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
