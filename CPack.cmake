919b2a30a83acdf7913610f3f976740df0136163
TiilS <ilyaagildin@yandex.ru>
2024-05-20 09:42:48 +0300

    Test

--- CPack.cmake
+++ CPack.cmake
@@ -1,33 +1,5 @@
-name: CPack
-
-on:
- push:
-   tags:
-     - v**
-
-jobs: 
-
-  build_packages_Linux:
-
-    runs-on: ubuntu-latest
-
-    steps:
-    - uses: actions/checkout@v4
-
-	- name: Config ConceptualExample
-	  run: cmake ${{github.workspace}} -B ${{github.workspace}}/build -D PRINT_VERSION=${GITHUB_REF_NAME#v}
-
-	- name: Build ConceptualExample
-	  run: cmake --build ${{github.workspace}}/build1
-
-    - name: Build package
-      run: cmake --build ${{github.workspace}}/build1 --target package
-
-    - name: Build source package
-      run: cmake --build ${{github.workspace}}/build1 --target package_source
-
-    - name: Make a release
-      uses: ncipollo/release-action@v1.10.0
-      with:
-        artifacts: "build/*.deb,build/*.tar.gz,build/*.zip"
-        token: ${{ secrets.GITHUB_TOKEN }}
+include(InstallRequiredSystemLibraries)
+set(CPACK_PACKAGE_NAME "YourProjectName")
+set(CPACK_PACKAGE_VERSION "1.0.0")
+set(CPACK_PACKAGE_CONTACT "you@example.com")
+include(CPack)
